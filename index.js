64cbfc0905e271365ccc95e77ef3152134ca8681
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-11 20:03:21 -0300

    update redirect for iOS

--- index.js
+++ index.js
@@ -25,15 +25,11 @@ app.get('/', (req, res) => {
         // Redirect to Google Play or open the app if installed
         // res.redirect('intent://portonet#Intent;scheme=portonet://abrirappportonet;package=com.yourapp.package;end');
         res.redirect(`intent://${scheme}#Intent;scheme=portonet;package=${appPackage};S.browser_fallback_url=${encodeURIComponent(playStoreUrl)};end`);
+    } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
+        res.redirect(`www.google.com.br`);
     } else {
         res.send('Sistema operacional não suportado.');
     }
-    // } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
-    //     // Redirect to App Store or open the app if installed
-    //     res.redirect('yourappscheme://');
-    // } else {
-    //     res.send('Sistema operacional não suportado.');
-    // }
 });
 
 const port = process.env.PORT || 3000;
