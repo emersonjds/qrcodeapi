76a899ece862ac21fe0d1058acf05bb6d9f71576
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-11 16:34:16 -0300

    add gitignore

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get('/redirect', (req, res) => {
+app.use(cors());
+
+app.get('/test', (req, res) => {
+    res.json('Hello World')
+});
+
+app.get('/', (req, res) => {
+
     const userAgent = req.headers['user-agent'].toLowerCase();
+    return console.log(userAgent);
+
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.headers.origin && ['http://localhost:3000', 'http://localhost:3001'].includes(req.headers.origin)) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
+
     if (userAgent.includes('android')) {
         // Redirect to Google Play or open the app if installed
         res.redirect('intent://yourapp#Intent;scheme=yourappscheme;package=com.yourapp.package;end');
-    } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
-        // Redirect to App Store or open the app if installed
-        res.redirect('yourappscheme://');
     } else {
         res.send('Sistema operacional não suportado.');
     }
+    // } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
+    //     // Redirect to App Store or open the app if installed
+    //     res.redirect('yourappscheme://');
+    // } else {
+    //     res.send('Sistema operacional não suportado.');
+    // }
 });
 
 const port = process.env.PORT || 3000;
