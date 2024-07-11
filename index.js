ee72fd29efb34b152bc153fc183ec10128a73495
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-11 20:46:18 -0300

    validate and recognize device

--- index.js
+++ index.js
@@ -8,28 +8,42 @@ app.get('/test', (req, res) => {
     res.json('Hello World')
 });
 
+
+
 app.get('/', (req, res) => {
 
-    console.log('HOUVE UMA REQUEST');
-
-    const userAgent = req.headers['user-agent'].toLowerCase();
-
-    const appPackage = 'br.com.portoseguro.portonetmobile';
-    const scheme = 'portonet://abrirappportonet';
-    const playStoreUrl = `https://play.google.com/store/apps/details?id=br.com.portoseguro.portonetmobile&hl=pt_BR`;
-
-    if (userAgent.includes('android')) {
-        console.log('VALIDAÇÃO DEVICE ANDROID');
-        // Redirect to Google Play or open the app if installed
-        // res.redirect('intent://portonet#Intent;scheme=portonet://abrirappportonet;package=com.yourapp.package;end');
-        res.redirect(`intent://${scheme}#Intent;scheme=portonet;package=${appPackage};S.browser_fallback_url=${encodeURIComponent(playStoreUrl)};end`);
-    } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
-        console.log('VALIDAÇÃO DEVICE IOS');
-        res.redirect(`www.google.com.br`);
+    console.log('REQUEST EXECUTADO')
+    const userAgent = req.headers['user-agent'];
+    if (/android/i.test(userAgent)) {
+        console.log('REQUEST ANDROID')
+        res.json({ device: 'android' });
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        console.log('REQUEST IOS')
+        res.json({ device: 'ios' });
     } else {
-        console.log('SISTEMA NAO SUPORTADO');
-        res.send('Sistema operacional não suportado.');
+        console.log('DESCONHECIDO')
+        res.json({ device: 'unknown' });
     }
+    // console.log('HOUVE UMA REQUEST');
+    //
+    // const userAgent = req.headers['user-agent'].toLowerCase();
+    //
+    // const appPackage = 'br.com.portoseguro.portonetmobile';
+    // const scheme = 'portonet://abrirappportonet';
+    // const playStoreUrl = `https://play.google.com/store/apps/details?id=br.com.portoseguro.portonetmobile&hl=pt_BR`;
+    //
+    // if (userAgent.includes('android')) {
+    //     console.log('VALIDAÇÃO DEVICE ANDROID');
+    //     // Redirect to Google Play or open the app if installed
+    //     // res.redirect('intent://portonet#Intent;scheme=portonet://abrirappportonet;package=com.yourapp.package;end');
+    //     res.redirect(`intent://${scheme}#Intent;scheme=portonet;package=${appPackage};S.browser_fallback_url=${encodeURIComponent(playStoreUrl)};end`);
+    // } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
+    //     console.log('VALIDAÇÃO DEVICE IOS');
+    //     res.redirect(`www.google.com.br`);
+    // } else {
+    //     console.log('SISTEMA NAO SUPORTADO');
+    //     res.send('Sistema operacional não suportado.');
+    // }
 });
 
 const port = process.env.PORT || 3000;
