6c224b0519cf01247b5e1bdc78d122882fa7c85a
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-11 20:24:28 -0300

    adjust url array

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.get('/test', (req, res) => {
 
 app.get('/', cors(corsOptions), (req, res) => {
 
+    console.log('HOUVE UMA REQUEST');
+
+    const urls = [
+        'http://localhost:3000',
+        'https://portonetqrcode.netlify.app/'
+    ]
+
     const userAgent = req.headers['user-agent'].toLowerCase();
 
     const appPackage = 'br.com.portoseguro.portonetmobile';
@@ -25,17 +32,20 @@ app.get('/', cors(corsOptions), (req, res) => {
     const playStoreUrl = `https://play.google.com/store/apps/details?id=br.com.portoseguro.portonetmobile&hl=pt_BR`;
 
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if (req.headers.origin && ['http://localhost:3000', 'http://localhost:3001'].includes(req.headers.origin)) {
+    if (req.headers.origin && urls.includes(req.headers.origin)) {
         res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     }
 
     if (userAgent.includes('android')) {
+        console.log('VALIDAÇÃO DEVICE ANDROID');
         // Redirect to Google Play or open the app if installed
         // res.redirect('intent://portonet#Intent;scheme=portonet://abrirappportonet;package=com.yourapp.package;end');
         res.redirect(`intent://${scheme}#Intent;scheme=portonet;package=${appPackage};S.browser_fallback_url=${encodeURIComponent(playStoreUrl)};end`);
     } else if (userAgent.includes('iphone') || userAgent.includes('ipad')) {
+        console.log('VALIDAÇÃO DEVICE IOS');
         res.redirect(`www.google.com.br`);
     } else {
+        console.log('SISTEMA NAO SUPORTADO');
         res.send('Sistema operacional não suportado.');
     }
 });
