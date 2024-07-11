564c02f4c6c14167df54df14327c7baee62684d9
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-11 20:33:36 -0300

    adjust cors validations

--- index.js
+++ index.js
@@ -2,39 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
-const corsOptions = {
-    origin: 'https://portonetqrcode.netlify.app/',//(https://your-client-app.com)
-    optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors())
 
 app.get('/test', (req, res) => {
     res.json('Hello World')
 });
 
-app.get('/', cors(corsOptions), (req, res) => {
+app.get('/', (req, res) => {
 
     console.log('HOUVE UMA REQUEST');
-
-    const urls = [
-        'http://localhost:3000',
-        'https://portonetqrcode.netlify.app/'
-    ]
 
     const userAgent = req.headers['user-agent'].toLowerCase();
 
     const appPackage = 'br.com.portoseguro.portonetmobile';
     const scheme = 'portonet://abrirappportonet';
     const playStoreUrl = `https://play.google.com/store/apps/details?id=br.com.portoseguro.portonetmobile&hl=pt_BR`;
-
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if (req.headers.origin && urls.includes(req.headers.origin)) {
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    }
 
     if (userAgent.includes('android')) {
         console.log('VALIDAÇÃO DEVICE ANDROID');
